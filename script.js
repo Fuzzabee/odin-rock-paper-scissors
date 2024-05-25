a99401fbefd06315f7b25821821717e7c6122f99
James Baker <jamesjbaker@tutanota.com>
2024-05-25 03:07:44 -0400

    Complete and tested getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Testing");
+function getComputerChoice() {
+    // Get a random number from 0 to 2
+    let random = Math.floor(( Math.random() * 3 ));
+
+    if ( random === 0 ) {
+        return "rock";
+    }
+
+    if ( random === 1 ) {
+        return "paper";
+    }
+
+    return "scissors";
+}
+
+// console.log("Testing getComputerChoice");
+// for ( let i = 0; i < 10; i++ ) {
+//     console.log( getComputerChoice() );
+// }
