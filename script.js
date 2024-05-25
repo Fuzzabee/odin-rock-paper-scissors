f0368b862b29e5a07cea64d96b354126e059694e
James Baker <jamesjbaker@tutanota.com>
2024-05-25 04:04:52 -0400

    Complete and tested playGame

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function playRound(humanChoice, computerChoice) {
     // Initial check for tie
     if (humanChoice === computerChoice) {
         console.log(`It's a tie! You both chose ${humanChoice}!`);
-        return false;
+        return "tie";
     }
 
     // Check if human wins
@@ -40,14 +40,48 @@ function playRound(humanChoice, computerChoice) {
         humanChoice === "Scissors" && computerChoice === "Paper" ||
         humanChoice === "Paper" && computerChoice === "Rock") {
             console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-            return true;
+            return "human";
         }
 
     // Otherwise computer wins
     console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    computerScore++;
-    return true;
+    return "computer";
+}
+
+function playGame() {
+    let roundsPlayed = 0;
+    let humanScore = 0;
+    let computerScore = 0;
+    let humanChoice = "";
+    let computerChoice = "";
+
+    while (roundsPlayed < 5) {
+        // Get weapons from computer and user
+        computerChoice = getComputerChoice();
+        humanChoice = getHumanChoice();
+
+        let roundWinner = playRound(humanChoice, computerChoice);
+        if (roundWinner === "human") {
+            humanScore++;
+        } else if (roundWinner === "computer") {
+            computerScore++;
+        }
+
+        roundsPlayed++;
+    }
+
+    // Declare winner
+    if (humanScore > computerScore) {
+        console.log("You won the game!!!");
+    } else if (computerScore > humanScore) {
+        console.log("You lost the game!!!");
+    } else {
+        console.log("It's a tie!!!")
+    }
+
+    console.log("Final Score:");
+    console.log(`\tYou: ${humanScore}`);
+    console.log(`\tComputer: ${computerScore}`);
 }
 
 /////////////
@@ -75,5 +109,4 @@ function playRound(humanChoice, computerChoice) {
 // Main //
 //////////
 
-let humanScore = 0;
-let computerScore = 0;
+playGame();
